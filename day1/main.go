c0b6d5b60b79ce2d2e22cb8e18957038d2413d2f
qcu87z <ashley.hughes@qcu87z.net>
2020-12-04 16:41:00 +1100

    day1 complete

--- day1/main.go
+++ day1/main.go
@@ -25,12 +25,24 @@ func getInts(path string) []int {
 }
 
 func main() {
+	// Part 1
 	ints := getInts("intput")
 	for _, j := range ints {
 		for _, k := range ints {
 			if j+k == 2020 {
 				fmt.Printf("%d + %d = 2020\n", j, k)
 				fmt.Printf("Which means %d * %d = %d\n", j, k, j*k)
+			}
+		}
+	}
+	// Part 2
+	for _, j := range ints {
+		for _, k := range ints {
+			for _, l := range ints {
+				if j+k+l == 2020 {
+					fmt.Printf("%d + %d + %d = %d\n", j, k, l, j+k+l)
+					fmt.Printf("Which means %d * %d * %d = %d\n", j, k, l, j*k*l)
+				}
 			}
 		}
 	}
